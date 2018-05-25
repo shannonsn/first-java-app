19e9433cf41204e811aa54f3b8fd128a60ad1fa0
shannon <shannonsn19@gmailcom>
2018-05-25 16:06:24 +0200

    global variables and combining strings to make code more readable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -27,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity*5);
+        String priceMessage = "Total" ;
+        String thankYouMessage = "Thank You!";
+        displayMessage(priceMessage +": R"+ quantity*5 +'\n' + thankYouMessage);
     }
 
     /**
@@ -60,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
